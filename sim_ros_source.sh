3bb9ea635f6e11adb579436d0f4ea4458e4d232b
ramz <saikrn112@gmail.com>
2018-03-31 00:58:17 +0530

    stable version

--- sim_ros_source.sh
+++ sim_ros_source.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 source ./flight_ws/devel/setup.zsh    #// (optional)
-source ./Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
+source ./Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build/posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
