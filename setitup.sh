bf56ebcadbe7a095d32f3da3ffbddd592def0998
ramz <saikrn112@gmail.com>
2018-04-24 02:28:14 +0530

    script update

--- setitup.sh
+++ setitup.sh
@@ -1,6 +1,7 @@
 #~/bin/bash
-git submodule init 
-git submodule update --recursive
+git submodule update --init --recursive
+git submodule foreach --recursive git fetch
+git submodule foreach git merge origin master
 cd ./flight_ws 
 chmod +x setitup.sh
 ./setitup.sh
