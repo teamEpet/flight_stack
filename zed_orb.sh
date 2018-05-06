380050781b666f530b0e8fdb65feaa812bbbc89a
ramz <saikrn112@gmail.com>
2018-05-06 15:14:13 +0530

    zed and orb integration

--- zed_orb.sh
+++ zed_orb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source zed_ws/devel/setup.bash
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/media/Extra/orb_ws/ORB_SLAM2/Examples/ROS/ORB_SLAM2
-roslaunch ORB_SLAM2 ros_mono_cam.launch
+# cd /media/Extra/orb_ws/ORB_SLAM2
+roslaunch zed_wrapper orb_stereo.launch
