b4bdf290800becbfe3b0fc6705f3e8e8427526ab
ramz <saikrn112@gmail.com>
2018-05-10 11:48:46 +0530

    better odometry parameters

--- zed_orb.sh
+++ zed_orb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source zed_ws/devel/setup.bash
+source zed_ws/devel/setup.zsh
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/media/Extra/orb_ws/ORB_SLAM2/Examples/ROS/ORB_SLAM2
 # cd /media/Extra/orb_ws/ORB_SLAM2
-roslaunch zed_wrapper orb_stereo.launch
+# roslaunch zed_wrapper orb_stereo.launch
