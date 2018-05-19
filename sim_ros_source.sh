4aca87a9b31ffbd6239eaf9bf1f9ccad4e1a9d41
ramz <saikrn112@gmail.com>
2018-05-20 02:07:41 +0530

    testing iris with vi

--- sim_ros_source.sh
+++ sim_ros_source.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-source ./flight_ws/devel/setup.zsh    #// (optional)
-source ./flight_ws/devel/setup.bash
+#source ./flight_ws/devel/setup.zsh    #// (optional)
+#source ./flight_ws/devel/setup.bash
 source ./Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build/posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
