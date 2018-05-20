c49d60000205d6b06291b368575fd55a1d733bb0
ramz <saikrn112@gmail.com>
2018-05-20 10:17:16 +0530

    decoupled sourcing

--- sim_ros_source.sh
+++ sim_ros_source.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
-#source ./flight_ws/devel/setup.zsh    #// (optional)
+source /opt/ros/kinetic/setup.zsh
+source ./flight_ws/devel/setup.zsh 
 #source ./flight_ws/devel/setup.bash
 source ./Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build/posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/rotors_description
